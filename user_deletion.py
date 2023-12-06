3a609187d1d98e6ad9a055a48b4177ff51c85874
Regina Kánya <96615693+RegiKanya@users.noreply.github.com>
2023-12-06 12:42:49 +0100

    Update user_deletion.py
    restructure and develop the code in new functions

--- user_deletion.py
+++ user_deletion.py
@@ -31,29 +31,11 @@ conn.sign_in()
 print("Connection status: signed in")
 
 # get info about all users on the site
-users_df = querying.get_users_dataframe(conn)
-# print(users_df)
+projects_df = querying.get_projects_dataframe(conn)
+projects_df['content_tpye'] = 'project'
 
-# get the user ID for the user being removed
-with open('user_removed.csv', 'r') as t:
-    users = pd.read_csv(t)
-
-users_to_remove = users['email'].tolist()
-for email_address in users_to_remove:
-    user_id_to_remove = users[users['email'] == email_address]['id'].to_list().pop()
-    print(f"User ID to remove: {user_id_to_remove}")
-
-# get the user ID for the user who will take over the ownership
-new_owner_id = (
-    users_df[users_df['siteRole'].str.contains('Administrator')]['id']
-    .to_list().pop()
-)
-print(f"New user ID to get ownership: {new_owner_id}")
-
-# get all content from the user being removed
 workbooks_df = querying.get_workbooks_dataframe(conn)
 workbooks_df['content_type'] = 'workbook'
-# print(workbooks_df)
 
 datasources_df = querying.get_datasources_dataframe(conn)
 datasources_df['content_type'] = 'datasource'
@@ -61,7 +43,8 @@ datasources_df['content_type'] = 'datasource'
 flows_df = querying.get_flows_dataframe(conn)
 flows_df['content_type'] = 'flow'
 
-content_dfs = [workbooks_df, datasources_df, flows_df]
+content_dfs = [projects_df, workbooks_df, datasources_df, flows_df]
+
 content_ownership_df = pd.DataFrame()
 
 
@@ -84,19 +67,60 @@ for index, content_df in enumerate(content_dfs):
         temp_df = temp_df[col_to_keep]
         content_ownership_df = pd.concat([content_ownership_df, temp_df])
 
-owner_swap_df = content_ownership_df[content_ownership_df['owner_id'] == user_id_to_remove]
+# get info about all users on the site
+users_df = querying.get_users_dataframe(conn)
 
-for index, row in owner_swap_df.itertuples():
-    response = change_content_owner(conn, row['content_type'], row['id'], new_owner_id)
-    print(response.json())
-
-# user no longer owns any content, it can be properly removed from the site
+# get the user ID for the user being removed
 with open('user_removed.csv', 'r') as t:
     users = pd.read_csv(t)
 
-for user in users:
-    user_id = [0]
-    conn.remove_user_from_site(user_id=user_id_to_remove)
+users_to_remove = users['email'].tolist()
 
-# end the connection
-conn.sign_out()
+for email_address in users_to_remove:
+    email_address = email_address.lower()
+    user_id_to_remove = users[users['email'] == email_address]['id'].to_list().pop()
+    # print(f"User ID to remove: {user_id_to_remove}")
+
+# get the user ID for the user who will take over the ownership
+new_owner_id = (users_df[users_df['siteRole'].str.contains('Administrator')]['id'].to_list().pop())
+# print(f"New user ID to get ownership: {new_owner_id}")
+
+owner_swap_df = content_ownership_df[content_ownership_df['owner_id'] == user_id_to_remove]
+
+
+def remove_user(con, local_users_df, owner_id=user_id_to_remove):
+    try:
+        email_to_remove = local_users_df[local_users_df['id'] == owner_id]['email'].iloc[0]
+
+        for email_to_remove in users_to_remove:
+            email_to_remove = email_to_remove.lower()
+            id_to_remove = local_users_df[local_users_df['email'] == email_to_remove]['id'].iloc[0]
+            response_b = con.remove_user_from_site(user_id=id_to_remove)
+            if response_b.status_code == 204:
+                print(f"User with email {email_to_remove} removed successfully.")
+            else:
+                # print(f"{email_to_remove} user has content. Run it separately to change the ownership")
+                try:
+                    for inx, row in enumerate(owner_swap_df.itertuples()):
+                        response_i = change_content_owner(conn, row.content_type, row.id, new_owner_id)
+                        print(response_i.json())
+                        if response_i.status_code == 200:
+                            print(f"Content ownership changed for content id {row.id}")
+                        else:
+                            print(f"Failed to change ownership for content id {row.id}")
+                except Exception as e:
+                    print(f"An error occurred: {str(e)}")
+    except Exception as ex:
+        print(f"An error occurred: {str(ex)}")
+
+
+def main():
+    try:
+        remove_user(conn, users_df, owner_id=user_id_to_remove)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        conn.sign_out()
+
+if __name__ == '__main__':
+    main()
