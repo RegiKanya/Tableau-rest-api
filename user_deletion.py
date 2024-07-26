de7e593e60d3a74bbe9266fef837e3a37af31649
Regina Kánya <96615693+RegiKanya@users.noreply.github.com>
2024-07-26 13:00:28 +0200

    Update user_deletion.py
    I changed the structure and how to put the pieces together

--- user_deletion.py
+++ user_deletion.py
@@ -1,26 +1,23 @@
+import logging
 from tableau_api_lib import TableauServerConnection
 from tableau_api_lib.utils import querying, flatten_dict_column
 import configparser
 import pandas as pd
 
 file_path = 'user_removed.csv'
+config_file = 'tableau.ini'
 
 # define Tableau Server config details
 config = configparser.ConfigParser()
-config.read('tableau.ini')
-
-token_name = config['tableau']['token_name']
-personal_access_token = config['tableau']['personal_access_token']
-server_url = config['tableau']['server_url']
-api_version = config['tableau']['api_version']
+config.read(config_file)
 
 config = {
     'tableau_prod': {
-        'server': server_url,
-        'api_version': api_version,
-        'personal_access_token_name': token_name,
-        'personal_access_token_secret': personal_access_token,
-        'site_name': '',
+        'server': config['tableau']['server_url'],
+        'api_version': config['tableau']['api_version'],
+        'personal_access_token_name': config['tableau']['token_name'],
+        'personal_access_token_secret': config['tableau']['personal_access_token'],
+        'site_name': config['tableau']['site_name'],
         'site_url': ''
     }
 }
@@ -28,97 +25,79 @@ config = {
 # establish connection with Tableau Server
 conn = TableauServerConnection(config)
 conn.sign_in()
-print("Connection status: signed in")
+logging.info("Connection status: signed in")
 
-# get info about all users on the site
-projects_df = querying.get_projects_dataframe(conn)
-projects_df['content_tpye'] = 'project'
 
-workbooks_df = querying.get_workbooks_dataframe(conn)
-workbooks_df['content_type'] = 'workbook'
+def get_content_ownership(conn):
+    content_df = [
+        querying.get_projects_dataframe(conn).assign(content_type='project'),
+        querying.get_workbooks_dataframe(conn).assign(content_type='workbook'),
+        querying.get_datasources_dataframe(conn).assign(content_type='datasource'),
+        querying.get_flows_dataframe(conn).assign(content_type='flow')
+    ]
 
-datasources_df = querying.get_datasources_dataframe(conn)
-datasources_df['content_type'] = 'datasource'
-
-flows_df = querying.get_flows_dataframe(conn)
-flows_df['content_type'] = 'flow'
-
-content_dfs = [projects_df, workbooks_df, datasources_df, flows_df]
-
-content_ownership_df = pd.DataFrame()
+    content_ownership_df = pd.concat(content_df, ignore_index=True)
+    return flatten_dict_column(content_ownership_df, keys=['id'], col_name='owner')[['id', 'name', 'content_type', 'owner_id']]
 
 
 # change ownership of all content which the user being removed owns
-def change_content_owner(conn, content_type, content_id, new_owner_id):
+def change_content_owner(conn, content_type, content_id, l_new_owner_id):
+    if content_type == 'project':
+        return "Cannot update project owners via REST API (yet)."
     if content_type == 'workbook':
-        response = conn.update_workbook(workbook_id=content_id, new_owner_id=new_owner_id)
+        return conn.update_workbook(workbook_id=content_id, new_owner_id=l_new_owner_id)
     if content_type == 'datasource':
-        response = conn.update_datasource(datasource_id=content_id, new_owner_id=new_owner_id)
+        return conn.update_datasource(datasource_id=content_id, new_owner_id=l_new_owner_id)
     if content_type == 'flow':
-        response = conn.update_flow(flow_id=content_id, new_owner_id=new_owner_id)
-    return response
+        return conn.update_flow(flow_id=content_id, new_owner_id=l_new_owner_id)
+    return "Invalid content type."
 
 
-col_to_keep = ['id', 'name', 'content_type', 'owner_id']
-for index, content_df in enumerate(content_dfs):
-    if not content_df.empty:
-        temp_df = content_df.copy()
-        temp_df = flatten_dict_column(temp_df, keys=['id'], col_name='owner')
-        temp_df = temp_df[col_to_keep]
-        content_ownership_df = pd.concat([content_ownership_df, temp_df])
-
-# get info about all users on the site
-users_df = querying.get_users_dataframe(conn)
-
-# get the user ID for the user being removed
-with open('user_removed.csv', 'r') as t:
-    users = pd.read_csv(t)
-
-users_to_remove = users['email'].tolist()
-
-for email_address in users_to_remove:
-    email_address = email_address.lower()
-    user_id_to_remove = users[users['email'] == email_address]['id'].to_list().pop()
-    # print(f"User ID to remove: {user_id_to_remove}")
-
-# get the user ID for the user who will take over the ownership
-new_owner_id = (users_df[users_df['siteRole'].str.contains('Administrator')]['id'].to_list().pop())
-# print(f"New user ID to get ownership: {new_owner_id}")
-
-owner_swap_df = content_ownership_df[content_ownership_df['owner_id'] == user_id_to_remove]
-
-
-def remove_user(con, local_users_df, owner_id=user_id_to_remove):
+# changing the ownership and removing the user
+def remove_user(conn, users_df, email):
     try:
-        email_to_remove = local_users_df[local_users_df['id'] == owner_id]['email'].iloc[0]
-
-        for email_to_remove in users_to_remove:
-            email_to_remove = email_to_remove.lower()
-            id_to_remove = local_users_df[local_users_df['email'] == email_to_remove]['id'].iloc[0]
-            response_b = con.remove_user_from_site(user_id=id_to_remove)
-            if response_b.status_code == 204:
-                print(f"User with email {email_to_remove} removed successfully.")
-            else:
-                print(f"{email_to_remove} user has content. Run it separately to change the ownership")      
+        user_id = users_df.loc[users_df['email'].str.lower() == email.lower(), 'id'].values[0]
+        response = conn.remove_user_from_site(user_id=user_id)
+        if response.status_code == 204:
+            print(f"User with email {email} removed successfully.")
+        else:
+            print(f"Failed to remove user with email {email}. They may have content that needs reassignment.")
+    except IndexError:
+        logging.error(f"No user found with email {email}.")
     except Exception as ex:
-        print(f"An error occurred: {str(ex)}")
+        logging.error(f"An error occurred while removing user with email {email}: {ex}")
 
 
 def main():
     try:
-        for inx, row in enumerate(owner_swap_df.itertuples()):
-            response_i = change_content_owner(conn, row.content_type, row.id, new_owner_id)
-            print(response_i.json())
-            if response_i.status_code == 200:
-                print(f"Content ownership changed for content id {row.id}")
-            else:
-                print(f"Failed to change ownership for content id {row.id}")
+        # Get info about all users on the site
+        users_df = querying.get_users_dataframe(conn)
 
-        remove_user(conn, users_df, owner_id=user_id_to_remove)
+        # Read users to be removed
+        users_to_remove = pd.read_csv(file_path)['email'].str.lower().tolist()
+
+        # Get new owner ID (an Administrator)
+        new_owner_id = users_df.loc[users_df['siteRole'].str.contains('Administrator'), 'id'].values[0]
+
+        for email in users_to_remove:
+            user_id_to_remove = users_df.loc[users_df['email'] == email, 'id'].values[0]
+            owner_swap_df = get_content_ownership(conn)
+            owner_swap_df = owner_swap_df[owner_swap_df['owner_id'] == user_id_to_remove]
+
+            for row in owner_swap_df.itertuples():
+                response = change_content_owner(conn, row.content_type, row.id, new_owner_id)
+                if hasattr(response, 'status_code') and response.status_code == 200:
+                    logging.info(f"Content ownership changed for {row.content_type} ID {row.id}")
+                else:
+                    logging.error(f"Failed to change ownership for {row.content_type} ID {row.id}")
+
+            remove_user(conn, users_df, email)
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        logging.error(f"An error occurred: {str(e)}")
     finally:
         conn.sign_out()
+        logging.info("Connection closed.")
+
 
 if __name__ == '__main__':
     main()
